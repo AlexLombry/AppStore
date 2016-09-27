7206045c6f6c5ccfdef7384bcca22e1ef9b0d115
Alex Lombry <alex.lombry@gmail.com>
2016-09-27 16:04:04 +0200

    Add image, construct view

---
+++ AppStore/AppCell.swift
@@ -0,0 +1,77 @@
+//
+//  AppCell.swift
+//  AppStore
+//
+//  Created by Alex Lombry on 27/09/2016.
+//  Copyright © 2016 Alex Lombry. All rights reserved.
+//
+
+import UIKit
+
+class AppCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(codeer:) has not been implemented")
+    }
+    
+    // each image of the view
+    let imageView: UIImageView = {
+        let iv = UIImageView()
+        
+        iv.image = UIImage(named: "frozen")
+        iv.contentMode = .scaleAspectFill
+        iv.layer.cornerRadius = 16
+        iv.layer.masksToBounds = true
+        
+        return iv
+    }()
+    
+    // set the label for each image view
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        
+        label.text = "Disney Build It: Frozen"
+        label.font = .systemFont(ofSize: 14)
+        label.numberOfLines = 2
+        
+        return label
+    }()
+    
+    // set the label for each image view
+    let categoryLabel: UILabel = {
+        let label = UILabel()
+        
+        label.text = "Entertainment"
+        label.font = .systemFont(ofSize: 13)
+        label.textColor = .darkGray
+        
+        return label
+    }()
+    
+    // set the label for each image view
+    let priceLabel: UILabel = {
+        let label = UILabel()
+        
+        label.text = "3.99€"
+        label.font = .systemFont(ofSize: 13)
+        label.textColor = .darkGray
+        
+        return label
+    }()
+    
+    func setupViews() {
+        addSubview(imageView)
+        addSubview(nameLabel)
+        addSubview(categoryLabel)
+        addSubview(priceLabel)
+        
+        imageView.frame = CGRect(x: 0, y: 0, width: frame.width, height: frame.width)
+        nameLabel.frame = CGRect(x: 0, y: frame.width + 2, width: frame.width, height: 40)
+        categoryLabel.frame = CGRect(x: 0, y: frame.width + 38, width: frame.width, height: 20)
+        priceLabel.frame = CGRect(x: 0, y: frame.width + 56, width: frame.width, height: 20)
+    }
+}
--- AppStore/CategoryCell.swift
+++ AppStore/CategoryCell.swift
@@ -9,7 +9,6 @@
 import UIKit
 
 class CategoryCell: UICollectionViewCell, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
-
     
     private let cellId = "appCellId"
     
@@ -22,6 +21,16 @@ class CategoryCell: UICollectionViewCell, UICollectionViewDataSource, UICollecti
         fatalError("init(coder:) has not been implemented")
     }
     
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        
+        label.text = "Best New Apps"
+        label.font = .systemFont(ofSize: 16)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
     let appsCollectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
@@ -33,20 +42,31 @@ class CategoryCell: UICollectionViewCell, UICollectionViewDataSource, UICollecti
         
     }()
     
-    func setupViews() {
-        backgroundColor = .red
+    let dividerLineView: UIView = {
+        let view = UIView()
         
+        view.backgroundColor = UIColor(white: 0.4, alpha: 0.4)
+        
+        // mandatory, if not the app will crash
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+    
+    func setupViews() {
         addSubview(appsCollectionView)
+        addSubview(dividerLineView)
+        addSubview(nameLabel)
         
         appsCollectionView.dataSource = self
         appsCollectionView.delegate = self
-        
         appsCollectionView.register(AppCell.self, forCellWithReuseIdentifier: cellId)
         
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-14-[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-14-[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": dividerLineView]))
         addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": appsCollectionView]))
-        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": appsCollectionView]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[nameLabel(30)][v0][v1(0.5)]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": appsCollectionView, "v1": dividerLineView, "nameLabel": nameLabel]))
     }
-    
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 5
@@ -57,23 +77,11 @@ class CategoryCell: UICollectionViewCell, UICollectionViewDataSource, UICollecti
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 100, height: frame.height)
-        
+        return CGSize(width: 100, height: frame.height - 32)
     }
     
-}
-
-class AppCell: UICollectionViewCell {
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setupViews()
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsetsMake(0, 14, 0, 14)
     }
     
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(codeer:) has not been implemented")
-    }
-    
-    func setupViews() {
-        backgroundColor = .orange
-    }
 }
